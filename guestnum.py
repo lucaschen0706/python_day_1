b7328b8dd9b12aa9aac6334ca103b7a2f7b75625
Lucas chen <vincent.yeats@gmail.com>
2021-05-10 20:13:11 +0800

    final version

--- guestnum.py
+++ guestnum.py
@@ -1,7 +1,12 @@
 import random
 
-num = random.randint(1, 100)
+start = int(input('請決定隨機數字範圍開始值: '))
+end = int(input('請決定隨機數字範圍結束值: '))
+num = random.randint(start, end)
+count = 0
+
 while True:
+	count += 1
 	guest = int(input('猜猜看數字是多少: '))
 	if guest > num:
 		print('比答案大')
@@ -9,4 +14,6 @@ while True:
 		print('比答案小')
 	else:
 		print('終於猜對了!')
+		print('這是你猜的第', count, '次')
 		break
+	print('這是你猜的第', count, '次')
